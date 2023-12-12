ba60a070a35dfb08e55e3970c8d3e17d41436f68
zacharyGayford <zachary.gayford@gmail.com>
2023-12-11 20:24:42 -0600

    blocked out platform interface

---
+++ engine/src/platform/platform.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "defines.h"
+
+typedef struct {
+	void* internal;
+} platform_state;
+
+api b8 platform_create(
+		platform_state* state,
+		const char* applicationName,
+		i32 x,
+		i32 y,
+		i32 width,
+		i32 height);
+
+api void platform_destroy(platform_state* state);
+
+api b8 platform_poll_events(platform_state* state);
+
+void* platform_memory_allocate(u64 length, b8 aligned);
+void platform_memory_free(void* block, b8 aligned);
+void platform_memory_zero(void* dst, u64 length);
+void platform_memory_copy(void* dst, const void* src, u64 length);
+void platform_memory_set(void* dst, i32 value, u64 length);
+
+void platform_console_stdout(const char* message);
+void platform_console_stderr(const char* message);
+
+f64 platform_time_get();
+void platform_time_sleep(u64 ms);
